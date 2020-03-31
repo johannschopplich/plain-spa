e1df7d6156120bb9084df9f740205941435c9d84
Johann Schopplich <mail@johannschopplich.com>
2020-03-31 18:23:11 +0200

    Fix header intendation

--- js/App.js
+++ js/App.js
@@ -9,15 +9,15 @@ export default view => html`
 
 const Header = () => html`
   <header class="container is-sm pb-l">
-  <h1 class="title text-4 py-m">Vanilla SPA</h1>
-  <section>
-    <ul class="p-m bg-contrast-lowest">
-      <li><a href="/">Home</a></li>
-      <li><a href="/todo">Todo list</a></li>
-      <li><a href="/blog/post-with-name">Blog post with id</a></li>
-      <li><a href="/any-link" data-ignore-router>Ignore link (the page will reload)</a></li>
-      <li><a href="/not-found">404</a></li>
-    </ul>
-  </section>
+    <h1 class="title text-4 py-m">Vanilla SPA</h1>
+    <section>
+      <ul class="p-m bg-contrast-lowest">
+        <li><a href="/">Home</a></li>
+        <li><a href="/todo">Todo list</a></li>
+        <li><a href="/blog/post-with-name">Blog post with id</a></li>
+        <li><a href="/any-link" data-ignore-router>Ignore link (the page will reload)</a></li>
+        <li><a href="/not-found">404</a></li>
+      </ul>
+    </section>
   </header>
 `
