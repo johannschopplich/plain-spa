0fee9c0907cb8ab2d9e563073781d73a9d1aa924
Johann Schopplich <mail@johannschopplich.com>
2020-04-18 13:28:22 +0200

    Sticky header

--- js/App.js
+++ js/App.js
@@ -2,22 +2,26 @@ import { html } from './modules/sinuous/index.js'
 
 export default view => html`
   <${Header} />
-  <main class="box block is-" data-theme="light">
-    ${view}
+  <main>
+    <div class="box" data-theme="light">
+      ${view}
+    </div>
   </main>
 `
 
 const Header = () => html`
-  <header class="box mb-xs" data-theme="light">
-    <p class="title text-4">Vanilla SPA</p>
-    <nav>
-      <ul class="flow-h">
-        <li><a href="/">Home</a></li>
-        <li><a href="/todo">Todo</a></li>
-        <li><a href="/blog/post-with-id">Post with ID</a></li>
-        <li><a href="/any-link" data-ignore-router>Ignore Router Link</a></li>
-        <li><a href="/not-found">404</a></li>
-      </ul>
-    </nav>
+  <header>
+    <div class="box" data-theme="light">
+      <p class="title text-4">Vanilla SPA</p>
+      <nav>
+        <ul class="flow-h">
+          <li><a href="/">Home</a></li>
+          <li><a href="/todo">Todo</a></li>
+          <li><a href="/blog/post-with-id">Post with ID</a></li>
+          <li><a href="/any-link" data-ignore-router>Ignore Router Link</a></li>
+          <li><a href="/not-found">404</a></li>
+        </ul>
+      </nav>
+    </div>
   </header>
 `
