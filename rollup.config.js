17a65ac2c41973d178afbeb5f9fa3b1380a4836c
Johann Schopplich <mail@johannschopplich.com>
2020-04-19 17:41:04 +0200

    Optional bundling process for production environment

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import rootImport from 'rollup-plugin-root-import'
+import htmBabelPlugin from './js/modules/sinuous/babel-plugin-htm/index.js'
+
+const production = !process.env.ROLLUP_WATCH
+
+export default [{
+	input: 'js/main.js',
+	output: [
+		{
+			file: 'build/bundle.js',
+			format: 'iife'
+		}
+	],
+	plugins: [
+    rootImport({
+      root: `${__dirname}/js/`,
+      useInput: 'prepend',
+      extensions: '.js',
+    }),
+    babel({
+      plugins: [
+        [htmBabelPlugin, {
+          // import { h } from '/modules/sinuous/index.js'
+          import: '/modules/sinuous/index.js'
+        }]
+      ]
+    }),
+
+    // If building for production, minify
+    production && terser()
+  ],
+}]
