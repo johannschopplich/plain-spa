1bc7fae1369197e00c05d5e310d0eb39ddbfc7c7
Johann Schopplich <mail@johannschopplich.com>
2020-05-25 16:06:51 +0200

    Upgrade sinuous babel plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,31 @@
+import alias from '@rollup/plugin-alias'
 import babel from 'rollup-plugin-babel'
 import bundleSize from 'rollup-plugin-size'
 import htmBabelPlugin from './js/modules/sinuous/babel-plugin-htm/index.js'
-import rootImport from 'rollup-plugin-root-import'
 import { terser } from 'rollup-plugin-terser'
 
+const path = require('path')
 const production = !process.env.ROLLUP_WATCH
 
 export default {
-	input: 'js/main.js',
-	output: {
+  input: 'js/main.js',
+  output: {
     file: 'build/bundle.js',
     format: 'es'
   },
-	plugins: [
-    bundleSize(),
-    rootImport({
-      root: `${__dirname}/js/`,
-      useInput: 'prepend',
-      extensions: '.js'
+  plugins: [
+    alias({
+      entries: [
+        { find: '@', replacement: path.resolve(__dirname, 'js') }
+      ]
     }),
+    bundleSize(),
     babel({
       exclude: 'node_modules/**',
       plugins: [
         [htmBabelPlugin, {
           // `import { h } from '/modules/sinuous/index.js'`
-          import: '/modules/sinuous/index.js'
+          import: '@/modules/sinuous/index.js'
         }]
       ]
     }),
